844fe4aab7ddee7ad9d745cd3544cb22239b1eaa
omaremam182 <114952902+omaremam182@users.noreply.github.com>
2024-11-02 13:39:46 +0300

    Add files via upload

---
+++ firebase_config.py
@@ -0,0 +1,9 @@
+firebaseConfig = {
+    "apiKey": "AIzaSyAUesDUcgX8JKDvboFdcaN6j9ki5zwZ4QM",
+    "authDomain": "flask-chats.firebaseapp.com",
+    "projectId": "flask-chats",
+    "storageBucket": "flask-chats.firebasestorage.app",
+    "messagingSenderId": "276919289662",
+    "appId": "1:276919289662:web:5ec0484a643104d4648e6f",
+    "measurementId": "G-5PHTF493L3"
+}
